5aa6fd414510d6b00564c13465252f55dffcfab5
Александр Халезин <iskandersunset@gmail.com>
2022-10-13 12:22:26 +0500

    Финиш асерты

--- test_12_dynamic_property.py
+++ test_12_dynamic_property.py
@@ -10,21 +10,38 @@ from selenium.webdriver.common.keys import Keys
 # driver = webdriver.Chrome(executable_path='C:\\_teach\\Silenium\\chromedriver.exe')
 s = Service('C:/_teach/Silenium/chromedriver.exe')
 driver = webdriver.Chrome(service=s)
-base_url = 'https://demoqa.com/dynamic-properties'
+base_url = 'https://demoqa.com/radio-button'
 # login_standard_user = "standard_user"
 # login_password_user = "secret_sauce"
 driver.get(base_url)
 # driver.maximize_window()
 
-
+yes_radio = driver.find_element(By.XPATH, "//label[@class='custom-control-label']")
+yes_radio.click()
+time.sleep(2)
 try:
-    visible_button = driver.find_element(By.XPATH, "//button[@id='visibleAfter']")
-    visible_button.click()
-except NoSuchElementException as exeption:
-    print('Ошибка NoSuchElementException')
-    time.sleep(10)
-    visible_button = driver.find_element(By.XPATH, "//button[@id='visibleAfter']")
-    visible_button.click()
-    print('Ткнули по кнопке')
+    message = driver.find_element(By.XPATH, "//span[@class='text-success']")
+    value_measage = message.text
+    print(value_measage)
+    assert value_measage == 'No'
+except AssertionError as exception:
+    driver.refresh()
+    yes_radio = driver.find_element(By.XPATH, "//label[@class='custom-control-label']")
+    yes_radio.click()
+    message = driver.find_element(By.XPATH, "//span[@class='text-success']")
+    value_measage = message.text
+    print('Radio button YES')
+print('Over test')
+
+
+# try:
+#     visible_button = driver.find_element(By.XPATH, "//button[@id='visibleAfter']")
+#     visible_button.click()
+# except NoSuchElementException as exeption:
+#     print('Ошибка NoSuchElementException')
+#     time.sleep(10)
+#     visible_button = driver.find_element(By.XPATH, "//button[@id='visibleAfter']")
+#     visible_button.click()
+#     print('Ткнули по кнопке')
 
 
