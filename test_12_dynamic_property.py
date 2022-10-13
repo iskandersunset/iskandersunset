d084886074b373dd8e2233589b34fb5050842381
Александр Халезин <iskandersunset@gmail.com>
2022-10-13 11:58:10 +0500

    Добавили трай ехепшион

--- test_12_dynamic_property.py
+++ test_12_dynamic_property.py
@@ -1,6 +1,7 @@
 import time
 
 from selenium import webdriver
+from selenium.common import NoSuchElementException
 from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -15,18 +16,15 @@ base_url = 'https://demoqa.com/dynamic-properties'
 driver.get(base_url)
 # driver.maximize_window()
 
-action = ActionChains(driver)
-elements_button = driver.find_element(By.XPATH, "//button[@id='doubleClickBtn']")
-action.double_click(elements_button).perform()
-result_duble_click = driver.find_element(By.XPATH, "//p[@id='doubleClickMessage']").text
-assert result_duble_click == 'You have done a double click'
-print('Двжды кликнули')
-time.sleep(2)
 
-action = ActionChains(driver)
-elements_button = driver.find_element(By.XPATH, "//button[@id='rightClickBtn']")
-action.context_click(elements_button).perform()
-result_context_click = driver.find_element(By.XPATH, "//p[@id='rightClickMessage']").text
-assert result_context_click == 'You have done a right click'
-print('Правой кликнули')
-time.sleep(2)
+try:
+    visible_button = driver.find_element(By.XPATH, "//button[@id='visibleAfter']")
+    visible_button.click()
+except NoSuchElementException as exeption:
+    print('Ошибка NoSuchElementException')
+    time.sleep(10)
+    visible_button = driver.find_element(By.XPATH, "//button[@id='visibleAfter']")
+    visible_button.click()
+    print('Ткнули по кнопке')
+
+
