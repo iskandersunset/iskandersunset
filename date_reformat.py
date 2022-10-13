deb5dc1d3e305e99dc5ec7ca6fb818edfb997559
Халезин Александр <iskandersunset@gmail.com>
2022-10-13 09:05:14 +0500

    удалили ненужен

--- date_reformat.py
+++
@@ -1,7 +0,0 @@
-import datetime
-
-today_date = datetime.datetime.utcnow().strftime("%d/%M/%Y")
-print(today_date)
-need_date = int(today_date[:2]), (today_date[1:3], today_date[])
-# print(type(today_date))
-print(need_date)
