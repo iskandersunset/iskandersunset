e8b84af6b9d5f8c797976b81adec5d24d6c84c85
Александр Халезин <iskandersunset@gmail.com>
2022-10-14 11:42:18 +0500

    используем WebDriverWEb

--- test_13_yavnie_neyavnie.py
+++ test_13_yavnie_neyavnie.py
@@ -6,6 +6,8 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # driver = webdriver.Chrome(executable_path='C:\\_teach\\Silenium\\chromedriver.exe')
 s = Service('C:/_teach/Silenium/chromedriver.exe')
@@ -13,10 +15,16 @@ driver = webdriver.Chrome(service=s)
 base_url = 'https://demoqa.com/dynamic-properties'
 driver.get(base_url)
 # driver.maximize_window()
-driver.implicitly_wait(10)
+# driver.implicitly_wait(10)
+
+
+# print('Start test')
+# visible_button = driver.find_element(By.XPATH, "//button[@id='visibleAfter']")
+# visible_button.click()
+# print('Finish test')
 
 
 print('Start test')
-visible_button = driver.find_element(By.XPATH, "//button[@id='visibleAfter']")
+visible_button = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//button[@id='visibleAfter']")))
 visible_button.click()
 print('Finish test')
