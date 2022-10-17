4d5db2fbc2dd0d60c08ec29a6e600c84ee0096e2
Александр Халезин <iskandersunset@gmail.com>
2022-10-17 16:37:18 +0500

    Малость поправили оформление вывода

--- DZ/test_6_smoke_test.py
+++ DZ/test_6_smoke_test.py
@@ -7,7 +7,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 # driver = webdriver.Chrome(executable_path='C:\\_teach\\Silenium\\chromedriver.exe')
-s = Service('C:/Users/iskan/PycharmProjects/Selenium/chromedriver.exe')
+# s = Service('C:/Users/iskan/PycharmProjects/Selenium/chromedriver.exe')
+s = Service('C:/_teach/Silenium/chromedriver.exe')
 driver = webdriver.Chrome(service=s)
 base_url = 'https://www.saucedemo.com/'
 login_standard_user = "standard_user"
@@ -17,26 +18,27 @@ driver.get(base_url)
 
 user_name = driver.find_element(By.XPATH, "//input[@id='user-name']")
 user_name.send_keys(login_standard_user)
-print("Input login")
+print("Ввели логин")
 time.sleep(1)
 
 user_pass = driver.find_element(By.XPATH, "//input[@id='password']")
 user_pass.send_keys(login_password_user)
-print("Input pass")
+print("Ввели пароль")
 time.sleep(1)
 
 user_pass.send_keys(Keys.RETURN)
-print("RETURN")
-time.sleep(1)
+print('Нажали Еnter\n', '--' * 20)
+time.sleep(2)
 
-"""Информация о первом товаре"""
+# Информация о первом товаре
 product_1 = driver .find_element(By.XPATH, "//a[@id='item_4_title_link']")
 value_product_1 = product_1.text
-print(value_product_1)
+print('Наименование: ', value_product_1)
 
 price_product_1 = driver.find_element(By.XPATH, "//*[@id='inventory_container']/div/div[1]/div[2]/div[2]/div")
 value_price_product_1 = price_product_1.text
-print(value_price_product_1)
+print('Цена товара: ', value_price_product_1)
+print('--' * 20)
 
 select_product_1 = driver.find_element(By.XPATH, "//button[@id='add-to-cart-sauce-labs-backpack']")
 value_select_product_1 = select_product_1.text
@@ -46,56 +48,59 @@ print('Clik добавить в корзину')
 carpet = driver.find_element(By.XPATH, "//*[@id='shopping_cart_container']/a")
 carpet.click()
 print('Click по корзине')
+print('--' * 20)
 
-"""Информация по товару 1 в корзине"""
+# Информация по товару в корзине
 cart_product_1 = driver.find_element(By.XPATH, "//a[@id='item_4_title_link']")
 value_cart_product_1 = cart_product_1.text
-print(value_cart_product_1)
+print('Наименование товара в корзине:  ', value_cart_product_1)
 assert value_product_1 == value_cart_product_1
-print('Инфо о товаре 1 гуд')
+print('Инфо о товаре совпадает с выбранным \n', '--' * 20)
 
 price_cart_product_1 = driver.find_element(By.XPATH, "//*[@id='cart_contents_container']/div/div[1]/div[3]/div[2]/div[2]/div")
 value_price_cart_product_1 = price_cart_product_1.text
-print(value_price_cart_product_1)
+print('Цена на товар в корзине: ', value_price_cart_product_1)
 assert value_price_product_1 == value_price_cart_product_1
-print('Инфо о цене на товара совпадает')
+print('Цена товара совпадает \n', '--' * 20)
 
 checkout = driver.find_element(By.XPATH, "//button[@id='checkout']")
 checkout.click()
-print('Клик по кнопке checkout')
+print('Клик по кнопке checkout \n', '--' * 20)
 
-"""Подтверждение инфо о покупателе"""
+# Вносим данные о покупателе
 first_name = driver.find_element(By.XPATH, "//input[@id='first-name']")
-first_name.send_keys('ПокупательИмя')
+first_name.send_keys('Имя_покупателя')
 print("Ввели имя покупателя")
 time.sleep(1)
 
 last_name = driver.find_element(By.XPATH, "//input[@id='last-name']")
-last_name.send_keys('ФамилияПокупателя')
+last_name.send_keys('Фамилия_покупателя')
 print("Ввели фамилию покупателя")
 time.sleep(1)
 
 postal_code = driver.find_element(By.XPATH, "//input[@id='postal-code']")
-postal_code.send_keys('ПочтовыйИндекс')
+postal_code.send_keys('Почтовый_индекс')
 print("Ввели индекс покупателя")
+print('--' * 20)
 time.sleep(1)
 
 button_continue = driver.find_element(By.XPATH, "//input[@id='continue']")
 button_continue.click()
 print('Клик по кнопке continue')
+print('--' * 20)
 
-"""Сверяем результаты цены и описания товара"""
+# Сверка наименования товара и цены в чеке
 over_product_1 = driver.find_element(By.XPATH, "//a[@id='item_4_title_link']")
 value_over_product_1 = over_product_1.text
 print(value_over_product_1)
 assert value_product_1 == value_over_product_1
-print('Инфо о товаре 1 гуд')
+print('Наименование товара совпадает с исходным')
 
 price_over_product_1 = driver.find_element(By.XPATH, "//*[@id='checkout_summary_container']/div/div[1]/div[3]/div[2]/div[2]/div")
 value_price_over_product_1 = price_over_product_1.text
 print(value_price_over_product_1)
 assert value_price_product_1 == value_price_over_product_1
-print('Инфо о цене на товара совпадает')
+print('Цена на товар совпадает \n' + '--' * 20)
 
 button_finish = driver.find_element(By.XPATH, "//button[@id='finish']")
 button_finish.click()
